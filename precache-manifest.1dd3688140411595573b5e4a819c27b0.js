3d57b7d0b8c65d92826276068fcf66102d27a9d8
soumitrafd <soumitra.fd@gmail.com>
2020-10-04 21:11:17 +0530

    Updates

---
+++ precache-manifest.1dd3688140411595573b5e4a819c27b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a834a3914ff52f6e6b0e63176fd410b0",
+    "url": "/dummy-data/index.html"
+  },
+  {
+    "revision": "6ac675a78cfba605f190",
+    "url": "/dummy-data/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "86c348f57772b7459d6e",
+    "url": "/dummy-data/static/css/main.4b646324.chunk.css"
+  },
+  {
+    "revision": "6ac675a78cfba605f190",
+    "url": "/dummy-data/static/js/2.39d06a07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dummy-data/static/js/2.39d06a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c348f57772b7459d6e",
+    "url": "/dummy-data/static/js/main.b20e9884.chunk.js"
+  },
+  {
+    "revision": "8eae8f8a23276d0ab927",
+    "url": "/dummy-data/static/js/runtime-main.4b1a8959.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dummy-data/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e344d76427deaf5478ba4a222a94574.js"
+  "/dummy-data/precache-manifest.1dd3688140411595573b5e4a819c27b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dummy-data/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
